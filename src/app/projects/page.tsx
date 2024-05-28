7889f66ab2d6260bec87f5370a21f118d6ee8d62
EzzatEsam <ezzatesam1214@gmail.com>
2024-05-28 21:12:20 +0300

    fix ?

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,11 +7,14 @@ import ProjectCard from "@/components/ProjectCard";
 import { useState, useEffect } from "react";
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
+  if (typeof window !== "undefined") {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+  return { width: 0, height: 0 };
 }
 
 function useWindowDimensions() {
